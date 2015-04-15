621940efa2b1b9697758e8cc4c88e7e4f085e762
sinan <sinangoo@gmx.de>
2015-04-15 22:21:01 +0200

    Further dagger dependency experiment

--- dagger_experiments/src/main/java/something/MainModule.java
+++ dagger_experiments/src/main/java/something/MainModule.java
@@ -8,13 +8,19 @@ import javax.inject.Singleton;
     injects = {
         Runner.class,
         Walker.class
-    }
+    },
+    library = true
 )
 public class MainModule {
 
     @Provides @Singleton Printer providePrinter() {
         System.out.println("providePrinter");
         return new Printer();
+    }
+
+    @Provides Walker provideWalker() {
+        System.out.println("provideWalker");
+        return new Walker(new Printer());
     }
 
 }
