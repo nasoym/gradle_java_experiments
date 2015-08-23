e2f0001161329fd34cce71f073c5419c8c290ba6
sinan <sinangoo@gmx.de>
2015-08-23 10:09:53 +0200

    Add basic prime factor project

---
+++ prime_factors/src/test/java/prime/PrimeNumberTest.java
@@ -0,0 +1,17 @@
+package prime;
+
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
