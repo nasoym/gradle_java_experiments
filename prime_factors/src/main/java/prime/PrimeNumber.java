ce023ddd558a185e6bed67def216f401a02f259b
sinan <sinangoo@gmx.de>
2015-08-23 10:34:27 +0200

    Add basic prime number detection

--- prime_factors/src/main/java/prime/PrimeNumber.java
+++ prime_factors/src/main/java/prime/PrimeNumber.java
@@ -2,4 +2,21 @@ package prime;
 
 public class PrimeNumber {
 
+  static public boolean isPrimeNumber(int number) {
+    if (number < 2 ) {
+      return false;
+    }
+    else {
+      /* for(int divisor = number-1; divisor > 2; divisor++ ) { */
+      int divisor = 2;
+      while(divisor < number) {
+        if ((number % divisor) == 0) {
+          return false;
+        }
+        divisor++;
+      }
+      return true;
+    }
+  }
+
 }
--- prime_factors/src/test/java/prime/PrimeNumberTest.java
+++ prime_factors/src/test/java/prime/PrimeNumberTest.java
@@ -9,9 +9,26 @@ import org.junit.Test;
 
 public class PrimeNumberTest {
 
-    @Test
-    public void test() {
+  @Test
+  public void zero_and_one_are_no_prime_numbers() {
+    assertThat(PrimeNumber.isPrimeNumber(0), is(false));
+    assertThat(PrimeNumber.isPrimeNumber(1), is(false));
+  }
 
+  @Test
+  public void test_basic_prime_numbers() {
+    int[] numbers = new int[]{2,3,5,7,11};
+    for(int primeNumber: numbers) {
+      assertThat(PrimeNumber.isPrimeNumber(primeNumber), is(true));
     }
+  }
+
+  @Test
+  public void test_basic_non_prime_numbers() {
+    int[] numbers = new int[]{4,6,8,9,10};
+    for(int nonPrimeNumber: numbers) {
+      assertThat(PrimeNumber.isPrimeNumber(nonPrimeNumber), is(false));
+    }
+  }
 
 }
